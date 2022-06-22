65d6cd1d58957ae9b7e9a8aa1686eeafe622426b
Stephanie <strejos30711@ufide.ac.cr>
2022-06-21 19:14:52 -0600

    Semana 5

--- app/src/main/java/com/example/lugares/ui/lugar/LugarFragment.kt
+++ app/src/main/java/com/example/lugares/ui/lugar/LugarFragment.kt
@@ -7,13 +7,15 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.lugares.R
+import com.example.lugares.adapter.LugarAdapter
 import com.example.lugares.databinding.FragmentAddLugarBinding
 import com.example.lugares.databinding.FragmentLugarBinding
 import com.example.lugares.viewmodel.LugarViewModel
 
 
-class LugarFragment : Fragment(){
+class LugarFragment : Fragment() {
     private lateinit var lugarViewModel: LugarViewModel
     private var _binding: FragmentLugarBinding? = null
     private val binding get() = _binding!!
@@ -26,18 +28,22 @@ class LugarFragment : Fragment(){
         lugarViewModel =
             ViewModelProvider(this).get(LugarViewModel::class.java)
 
-        _binding = FragmentLugarBinding.inflate(inflater,container, false)
+        _binding = FragmentLugarBinding.inflate(inflater, container, false)
 
         binding.addLugarFabButton.setOnClickListener {
             findNavController().navigate(R.id.action_nav_lugar_to_addLugarFragment)
+
+        }
+
+        val lugarAdapter = LugarAdapter()
+        val reciclador = binding.reciclador
+        reciclador.adapter = lugarAdapter
+        reciclador.layoutManager = LinearLayoutManager(requireContext())
+        lugarViewModel = ViewModelProvider(this)[LugarViewModel::class.java]
+        lugarViewModel.getAllData.observe(viewLifecycleOwner) { lugares ->
+            lugarAdapter.setData(lugares)
         }
         return binding.root
-    }
 
-
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
